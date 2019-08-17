8d7ddff84781caf11e12f3d2b7ea84dd2573c22b
kk <kk@kk-PC>
2019-08-17 14:47:05 +0800

    加了注释

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor//无参构造
 @AllArgsConstructor//全参构造
 public class Item {
+	//编号
 	private Integer id;//商品id
 	private String name;//商品名
 	private Integer number;//商品数量
