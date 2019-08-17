37ce2ddbc992099c25dfddddb77fe2eed9587d94
13974998269 <53943234+13974998269@users.noreply.github.com>
2019-08-17 15:39:28 +0800

    Update Item.java

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -9,13 +9,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Item {
 
-	//±àºÅ
-	private Integer id;//ÉÌÆ·id
-	private String name;//ÉÌÆ·Ãû
-	private Integer number;//ÉÌÆ·ÊıÁ¿
+	//ç¼–å·
+	private Integer id;//å•†å“id
+	private String name;//å•†å“å
+	private Integer number;//å•†å“æ•°é‡
 	
 	//333333
 	//2222222222222222
+	//4444444444
 }
 
 
